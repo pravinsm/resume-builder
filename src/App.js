cdb8874662c3efa72770f3fd6a75ef4f72f6d0ef
pravin sm <pravinsm@netart.io>
2021-06-17 01:37:19 +0530

    logo and about icons updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useRef } from "react";
+import React, { useEffect, useContext, useRef, useState } from "react";
 import { ContextGlobal } from "./context";
 import Menu from "./Components/Menu";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
@@ -8,6 +8,8 @@ import Template1 from "./Components/Templates/Template1";
 import domtoimage from "dom-to-image";
 import { jsPDF } from "../node_modules/jspdf/dist/jspdf.es";
 import FileSaver from "file-saver";
+import { GitHub, LinkedIn, Mail, Person } from "@material-ui/icons";
+import { SpeedDial, SpeedDialAction } from "@material-ui/lab";
 
 const useStyles = makeStyles(() => ({
   ContentWrapper: {
@@ -24,6 +26,15 @@ const useStyles = makeStyles(() => ({
     padding: "75px",
     minHeight: "1700px",
   },
+  logo_resumebuilder: {
+    fontFamily: "Sofia",
+    width: "max-content",
+    position: "absolute",
+    top: "0",
+    left: "15px",
+    margin: "10px 0",
+    fontSize: "30px",
+  },
 }));
 
 function App() {
@@ -31,6 +42,8 @@ function App() {
   const classes = useStyles();
   const fontRef = useRef(null);
   const loadRef = useRef(null);
+
+  const [open, setOpen] = useState(false);
 
   const theme = createMuiTheme({
     palette: {
@@ -83,9 +96,55 @@ function App() {
     loadRef.current.click();
   };
 
+  const actions = [
+    {
+      icon: <LinkedIn />,
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/pravin-s-m-128610165",
+    },
+    { icon: <GitHub />, name: "Github", link: "https://github.com/pravinsm" },
+    { icon: <Mail />, name: "Mail", link: "mailto:pravinsm10@gmail.com" },
+  ];
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App" style={{ margin: "2.5rem 0" }}>
+        <span className={classes.logo_resumebuilder + " font-effect-emboss"}>
+          Resume Builder
+        </span>
+        <div className="logo_Wrapper">
+          <SpeedDial
+            ariaLabel="SpeedDial"
+            className="speedDial"
+            hidden={false}
+            icon={<Person />}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            open={open}
+            direction={"left"}
+          >
+            {actions.map((action) => (
+              <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+                onClick={() => {
+                  setOpen(false);
+                  window.open(action.link);
+                }}
+              />
+            ))}
+          </SpeedDial>
+        </div>
+
         <Menu print={printDocument} load={loadDocument} save={saveDocument} />
 
         <div className={classes.ContentWrapper}>
